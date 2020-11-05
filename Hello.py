9cec5a767e25616657b8d7036c9bf31f8f86c266
Oliwia Wdowiak <234801@edu.p.lodz.pl>
2020-11-05 17:18:06 +0100

    guessgame

--- Hello.py
+++ Hello.py
@@ -1,9 +1,17 @@
-print("hello")
-print("goodbye")
-print('such a nice day')
-print('aaa')
-print ('bbb')
-print('Ala ma kota')
-print("ala")
-2==2
-print("buuuu")
+secret_word = "apple"
+user_word = ""
+user_word_count = 0
+user_word_limit = 3
+attemps = False
+
+while user_word != secret_word and not(attemps):
+    if user_word_count < user_word_limit:
+        user_word = input("Enter a word: ")
+        user_word_count += 1
+    else:
+        attemps = True
+
+if attemps:
+    print("You lost")
+else:
+    print("You win")
